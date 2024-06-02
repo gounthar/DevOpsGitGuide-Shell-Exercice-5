d6ddcfcdebf0f0747fa4c8f4b47b9b2447ef5bbc
gounthar <gounthar@gmail.com>
2024-06-02 17:05:56 +0200

    feat(gitpod): Permissions.

--- setup.sql
+++ setup.sql
@@ -18,3 +18,8 @@ VALUES('The Fellowship of the Ring',1,1),
       ('The Two Towers',1,1), ('The Return of the King',1,1),
       ('The Sum of All Men',2,2), ('Brotherhood of the Wolf',2,2),
       ('Wizardborn',2,2), ('The Hobbbit',0,1);
+
+# Create example-user with password password
+CREATE USER 'example-user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON testdb.* TO 'example-user'@'localhost';
+FLUSH PRIVILEGES;
